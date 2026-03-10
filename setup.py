78ca694818fb568c621785113e6c92273aa54417
viru27 <viranegautam27@gmail.com>
2026-03-10 21:28:17 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT= '-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    THIS IS A SETUP STRUCTURE
+
+    '''
+
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n", "") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
+
+
+setup(
+name = "mlwork",
+version= "0.0.1",
+author= "VIRANE",
+author_email= "viranegautam27@gmail.com",
+packages= find_packages(),
+install_requires= get_requirements('requirements.txt')
+)

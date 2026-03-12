464aa5fa9b4d5fbe3b0a336ec1e3d2facfaa1b5f
viru27 <viranegautam27@gmail.com>
2026-03-12 16:24:45 +0530

    data ingestion done

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,50 @@
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path = os.path.join("artifact","train.csv")
+    test_data_path = os.path.join("artifact","test.csv")
+    raw_data_path = os.path.join("artifact","data.csv")
+
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config=DataIngestionConfig()
+    #this is done if our datat is stored in any data base to read it (code is wriiten here so to write i can create a mongodb or my_sql client in utils.py)
+    def initate_data_ingestion(self):
+        logging.info("Enter the data ingestion method")
+        try:
+            df= pd.read_csv("Notebook\Data\stud.csv")#here only i can write the mongodb or from where we want make the data read
+            logging.info("Exported and read the dataset as dataframe")
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data_path, index=False,header=True)
+            
+            logging.info("Train test split initiated")
+            
+            train_set,test_set =train_test_split(df, test_size=0.2,random_state=42)
+
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            
+            logging.info("Ingestion od datat is completed")
+
+            return(self.ingestion_config.train_data_path,
+                   self.ingestion_config.test_data_path)
+
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+
+if __name__=="__main__":
+    obj=DataIngestion()
+    obj.initate_data_ingestion()
+

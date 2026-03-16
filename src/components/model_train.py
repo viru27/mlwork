a8ce9daf5e74d42f721d61753337b9ea98ada1c2
viru27 <viranegautam27@gmail.com>
2026-03-16 13:22:30 +0530

    app working

--- src/components/model_train.py
+++ src/components/model_train.py
@@ -47,6 +47,7 @@ class ModelTrainer:
                 "CatBoosting Regressor": CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
+            #### use/create of yml or config file####
             params={
                 "Decision Tree": {
                     'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
